e9d833dad5069a262448f3ed5c990937dd04b009
zhenguo <zhenguo@glowing.com>
2021-11-12 14:12:08 +0800

    升级lsp-mode
    移除company-lsp，已经不再支持了

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,11 +1,7 @@
 (require-package 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; 配置company，作为lsp的补全前端
-(require-package 'company-lsp)
-
 (push 'company-yasnippet company-backends)
-(push 'company-lsp company-backends)    ; company-lsp的优先级要放高一点，不然会补全不了
 
 (global-set-key (kbd "M-c") 'company-complete)
 
